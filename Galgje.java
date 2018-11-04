a0619ee01f2d3d983cf1d063da73abdd900ae10d
Karwaichan <karwaichanstar@gmail.com>
2018-11-04 14:26:03 +0100

    Prototype 1v3 galgje

--- Galgje.java
+++ Galgje.java
@@ -1,42 +1,46 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Galgje {
 	public static void main(String [] args) {
 	Scanner input = new Scanner(System.in);
+	Random random = new Random();
 	String test = "papegaai"; //bekijk arraylist functionaliteit.
-	int counter = 0;
+	String raden ="";
+	
+	String[] woorden = { "auto", "guitaar", "sneeuw","peper","fruit" };
+	char[] randomGuess = woorden[random.nextInt(woorden.length)].toCharArray();
+	
+	
+	
+	int raadTeller = 1;
+	int raadLimiet = 6;
+	boolean raadPogingen = false;
 	
 	
 	System.out.println("\t#####-Welkom bij galgje!-####");
 	printMenu();
 	
 	Game:
-	while(true) {
-		
+	while(!raden.equals(test) && !raadPogingen) {
+			
+			if(raadTeller<=raadLimiet) {
 			System.out.println("\tVul een 'voorlopig' woord  in"); //zoek uit hoe je per letter kan controleren.
 			String gok = input.next();
-			
+			raadTeller++;
+			}else {
+				raadPogingen = true;
+			}
 	
-			if(gok.equals("2")) {
-				break;
-			}
-			if(gok.equals(test)) {
-				counter++;
-				System.out.println("\tJe hebt het prototype verslagen, pfff...big deal!");
-				break;
-			}
-			if(gok.equals("1")) {
-				getProgressie(counter);
-				continue Game;
-			}
-			getWoord();
-			//getPoppetje();
-			counter++;
+
 		}
+	if(raadPogingen) {
+		System.out.println("Je hebt verloren je mag niet meer raden");
+	}
 	System.out.println("\t#######################");
 	System.out.println("\t# Bedankt en tot Ooit!#");
 	System.out.println("\t#######################");
-	getProgressie(counter);
+	
 	}
 	
 	public static void printMenu() {
@@ -44,13 +48,7 @@ public class Galgje {
 		System.out.println("\t<DRUK> 1. Om progressie te zien.");
 		System.out.println("\t<DRUK> 2. Om te stoppen.");
 		System.out.println("");
-	}
-	public static void getProgressie(int counter) {
-		System.out.println("\tJe hebt in het spel "+counter+ " pogingen gedaan.");
-	}
-	public static void getWoord() {
-		System.out.println("");
-		System.out.println("Je hebt de volgende (??????? - letters) al geraden\n");
 		
 	}
+
 }
